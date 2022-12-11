4a9e95d8591652ed09197d803955b08400f6e9db
Ilia Rosseev <ilia19042006@gmail.com>
2022-12-11 13:02:08 +0300

    Кнопки помощи

--- main.py
+++ main.py
@@ -9,21 +9,20 @@ select = select()
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    #TODO: Реализовать bot_help
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     registration = types.KeyboardButton("Регистрация")
     information = types.KeyboardButton("Информация о боте")
-    bot_help = types.KeyboardButton("Помощь")
+    bot_help = types.KeyboardButton("Помощь в регистрации")
     markup.add(registration, information, bot_help)
     printy(message.chat.id, f"Привет {message.from_user.first_name}!", reply_markup=markup)
 
 def interface(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    #TODO: Реализовать rating, bot_help
+    #TODO: Реализовать rating
     if select.select_status(message.chat.id) == 0:
         rating = types.KeyboardButton("Просмотреть работы")
         information = types.KeyboardButton("Информация о боте")
-        bot_help = types.KeyboardButton("Помощь")
+        bot_help = types.KeyboardButton("Помощь ученику")
         markup.add(rating, information, bot_help)
         printy(message.chat.id, f"Возможности:", reply_markup=markup)
     #TODO: Реализовать new_quest, rating, bot_help
@@ -82,7 +81,6 @@ def check_text_message(message):
     if message.text == "Информация о боте":
         printy(message.chat.id, f"Бот создать специально для 44 Гимназии г.Пензы, для проверки знаний школьников")
         printy(message.chat.id, "Создатель бота: https://t.me/Mr_GoldSky")
-        interface(message)
     elif message.text == "Регистрация":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
         student = types.KeyboardButton("Ученик")
@@ -98,5 +96,28 @@ def check_text_message(message):
         registration_teacher(message)
     elif message.text == "Возможности":
         interface(message)
+    elif message.text == "Помощь в регистрации":
+        printy(message.chat.id, '''Помощь в регистрации: \n\
+        1. Нажмите кнопку "Регистрация" \n\
+        2. Выберете, кто вы. Учитель или Ученик. \n\
+        3. Введите ваше НАСТОЯЩЕЕ имя и фамилию. \n\
+        4. Введите класс в котором вы учитесь/преподаёте.''')
+    #TODO: Доделать помощь 
+    elif message.text == "Помощь ученику":
+        printy(message.chat.id, f'''Помощь ученику: \n\
+Нажав кнопку "Просмотреть работы" Можно будет посмотреть список заданных учителями работ. \n
+Символом {chr(9989)} отмечены выполненные работы.
+Символом {chr(9200)} отмечены работы, ожидающие выполнения. Рядом с ними написанно крайнее время сдачи. \n
+После того, как все ученики сдадут работы или закончится время, рядом с работой будет написан % решения и оценка.''')
+    elif message.text == "":
+        pass
+    elif message.text == "":
+        pass
+    elif message.text == "":
+        pass
+    elif message.text == "":
+        pass
+    elif message.text == "":
+        pass
 
 bot.polling(none_stop=True)
