95625829f050c57a56cc6846a8c7cff16dd21fdf
Ilia Rosseev <ilia19042006@gmail.com>
2023-02-27 19:40:11 +0300

    Comments

--- Telegram/config.py
+++ Telegram/config.py
@@ -1,7 +1,7 @@
-BOT_TOKEN = "5806618918:AAGPM8wm52hyM8kXA9mRihxlKfkZtYrQAZ4"
-TG_BASE_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/Telegram/base/telegram_db.sqlite"
-RESULT_BASE_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/result_base/result_base.sqlite"
-BASE = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/Base"
-B_D = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/B_D"
-URL = 'https://api.telegram.org/bot'
-RESULT_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/Rezult"
+BOT_TOKEN = "5806618918:AAGPM8wm52hyM8kXA9mRihxlKfkZtYrQAZ4" #Токен бота
+TG_BASE_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/Telegram/base/telegram_db.sqlite" #Путь к базе данных бота
+RESULT_BASE_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/result_base/result_base.sqlite" #Путь к базе данных результатов
+BASE = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/Base" #Путь к карточкам
+B_D = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/B_D" #Путь к txt вариантам
+URL = 'https://api.telegram.org/bot' #Telegram url
+RESULT_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/Rezult" #Путь к txt результатам
