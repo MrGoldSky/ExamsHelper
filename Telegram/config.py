4ae1408eb0af50a82f58ae91117fcbb33f9aa583
Ilia Rosseev <ilia19042006@gmail.com>
2024-06-12 14:24:16 +0300

    v1.2

--- Telegram/config.py
+++ Telegram/config.py
@@ -1,8 +1,8 @@
 BOT_TOKEN = "5806618918:AAGPM8wm52hyM8kXA9mRihxlKfkZtYrQAZ4" #Токен бота
-TG_BASE_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/Telegram/base/telegram_db.sqlite" #Путь к базе данных бота
-RESULT_BASE_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/result_base/result_base.sqlite" #Путь к базе данных результатов
-BASE = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/Base" #Путь к карточкам
-B_D = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/B_D" #Путь к txt вариантам
+TG_BASE_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project/Telegram/base/telegram_db.sqlite" #Путь к базе данных бота
+RESULT_BASE_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project/result_base/result_base.sqlite" #Путь к базе данных результатов
+BASE = "C:/Users/Mr_GoldSky_/Desktop/Git/Project/Base" #Путь к карточкам
+B_D = "C:/Users/Mr_GoldSky_/Desktop/Git/Project/B_D" #Путь к txt вариантам
 URL = 'https://api.telegram.org/bot' #Telegram url
-RESULT_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/Rezult" #Путь к txt результатам
-UI_PATH = 'C:/Users/Mr_GoldSky_/Desktop/Git/Project_school/Telegram/untitled.ui'
+RESULT_PATH = "C:/Users/Mr_GoldSky_/Desktop/Git/Project/Result" #Путь к txt результатам
+UI_PATH = 'C:/Users/Mr_GoldSky_/Desktop/Git/Project/Telegram/untitled.ui'
