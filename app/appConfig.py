223c3b6c78250e6e62556a5e37cf2d1ae28e54a6
Rosseev Ilya <68150241+MrGoldSky@users.noreply.github.com>
2025-03-22 14:32:46 +0300

    Update appConfig.py

--- app/appConfig.py
+++ app/appConfig.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "5806618918:AAGPM8wm52hyM8kXA9mRihxlKfkZtYrQAZ4" #Токен бота
+BOT_TOKEN = "YOUR BOT TOKEN" #Токен бота
 TG_BASE_PATH = "bot/base/telegram_db.sqlite" #Путь к базе данных бота
 RESULT_BASE_PATH = "results/base/result_base.sqlite" #Путь к базе данных результатов
 TASKS = "tasks/" #Путь к карточкам
@@ -10,7 +10,7 @@ STYLE_PATH = "app/ui/styles/style.css"
 TEMP = 'app/temp'
 QUEST_TIME = {1:3, 2:3, 3:3, 4:2, 5:4, 6:4, 7:5, 8:4, 9:6, 10:3,
              11:3, 12:6, 13:3, 14:3, 15:3, 16:5, 17:14, 18:6, 19:6,
-             20:8, 21:11, 22:7, 23:8, 24:18, 25:20, 26:35, 27:35}
+             20:8, 21:11, 22:7, 23:8, 24:18, 25:20, 26:35, 27:35} #Время отведённое на задания
 viewExams_UI_PATH = 'app/ui/viewExams.ui'
 createExams_UI_PATH = 'app/ui/createExams.ui'
 aboutWindow_UI_PATH = 'app/ui/about.ui'
